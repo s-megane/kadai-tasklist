b5cd1ce77e87cee257e196ca680d81e659dd1c0b
s.megane <nihonniegaowo@outlook.jp>
2020-10-03 12:10:44 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,26 +8,7 @@
     </head>
     <body>
         
-    <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
-
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li>{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-            </ul>
-                </div>
-            </nav>
-        </header>
+        @include('commons.navbar')
 
         <div class="container">
             @include("commons.error_tasks")
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,24 @@
 @extends("layouts.app")
 @section("content")
-    <div class = "center center jumbotron">
-        <div class = "text-center">
-            <h1>タスク管理のトップ</h1>
-            <p>ここではタスク管理を行うことができます。</p>
-            <p>初めての方は新規登録をして、あなただけのタスク管理を行いましょう！</p>
-            <div class = "mt-0 mb-3">
-                <p>↓初めての方↓</p>
-                    
-                {!! link_to_route('signup.get', '新規登録！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name . "のページです。" }}
+        
+    @else
+        <div class = "center center jumbotron">
+            <div class = "text-center">
+                <h1>タスク管理のトップ</h1>
+                <p>ここではタスク管理を行うことができます。</p>
+                <p>初めての方は新規登録をして、あなただけのタスク管理を行いましょう！</p>
+                <div class = "mt-0 mb-3">
+                    <p>↓初めての方↓</p>
+                        
+                    {!! link_to_route('signup.get', '新規登録！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                </div>
+                <div class = "mt-0 mb-3">
+                    <p>↓すでに登録済みの方はこちら↓</p>
+                    {!! link_to_route('login.post', 'ログインする！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                </div>
             </div>
-                <p>↓すでに登録済みの方はこちら↓</p>
-            
         </div>
-    </div>
+    @endif
 @endsection
